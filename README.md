a96cba0626222518d58bb55619857d8fdce2c645
g5becks <techstar.dev@hotmail.com>
2021-01-23 14:25:46 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Taskfile.js
+# Taskfile
+
+A node js cli wrapper around [Taskfile](https://taskfile.dev/#/).
+
+For more information on what a Taskfile is, visit <https://taskfile.dev/>
---
+++ bin/cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const spawn = require('child_process').spawn
+const path = require('path')
+
+const launch = () => {
+//   deepcode ignore IndirectCommandInjection
+  const child = spawn(path.join(__dirname, 'task'), process.argv.slice(2) ,{ stdio: [process.stdin, process.stdout, process.stderr] })
+
+  child.on('close', (code) => code !== 0 ? process.exit(1) : undefined)
+
+}
